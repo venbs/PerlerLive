f0190013006be58100a9a19c8e45b825a7ddd2e1
VenbsZhou <eeven66@gmail.com>
2026-03-27 21:20:28 +0800

    fix: set base path for github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+  base: '/PerlerStudio/',
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
